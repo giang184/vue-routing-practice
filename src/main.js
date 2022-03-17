5aca06733b94efdda37e8441c1aab784f5711ddf
Andrew Giang <giang184@gmail.com>
2022-03-17 15:04:15 -0700

    add multiple components

--- src/main.js
+++ src/main.js
@@ -6,14 +6,23 @@ import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
 import TeamMembers from './components/teams/TeamMembers'
 import NotFound from './components/nav/NotFound.vue'
+import TeamsFooter from './components/teams/TeamsFooter.vue'
+import UsersFooter from './components/users/UsersFooter.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {path: '/', redirect: '/teams'},
-    { path: '/teams', component: TeamsList },
-    { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/', redirect: '/teams' },
+    { 
+      name: 'teams',
+      path: '/teams',
+      components: {default: TeamsList, footer: TeamsFooter },
+      children: [
+        { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
+      ]
+    },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
+
     { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active'
